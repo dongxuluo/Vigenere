ebe81b1650b498ec9c08bb4ae18595551c7cb2bd
dongxuluo <dxluo.xdu@gmail.com>
2020-10-13 16:20:06 +0800

    Update Vigenere_crack.py

--- src/Vigenere_crack.py
+++ src/Vigenere_crack.py
@@ -1,15 +1,14 @@
-# 从文件中读入密文
 with open('../static/cipher.txt') as f:
     cipher_text_raw = f.read()
-# 去掉非字母
+
 cipher_text = ''
 for text in cipher_text_raw:
     if text.isalpha():
         cipher_text += text
-# 英文字符频率
-frequency_table = [0.0651738, 0.0124248, 0.0217339, 0.0349835, 0.1041442, 0.0197881, 0.0158610, 0.0492888, 0.0558094,
-                   0.0009033, 0.0050529, 0.0331490, 0.0202124, 0.0564513, 0.0596302, 0.0137645, 0.0008606, 0.0497563,
-                   0.0515760, 0.0729357, 0.0225134, 0.0082903, 0.0171272, 0.0013692, 0.0145984, 0.0007836]
+# 英文字符频率列表
+frequency_list = [0.0651738, 0.0124248, 0.0217339, 0.0349835, 0.1041442, 0.0197881, 0.0158610, 0.0492888, 0.0558094,
+                  0.0009033, 0.0050529, 0.0331490, 0.0202124, 0.0564513, 0.0596302, 0.0137645, 0.0008606, 0.0497563,
+                  0.0515760, 0.0729357, 0.0225134, 0.0082903, 0.0171272, 0.0013692, 0.0145984, 0.0007836]
 
 
 # 计算单个密钥 k 的重合指数
@@ -20,7 +19,7 @@ def coincidence_index(cipher, k):
         x[ord(letter) - ord('a') - k] += 1
     CI = 0
     for i in range(26):
-        CI += (x[i] / L) * frequency_table[i]
+        CI += (x[i] / L) * frequency_list[i]
     return CI
 
 
@@ -37,15 +36,15 @@ def crack_key(cipher, key_len, num):
 
 # 分组破解,并找出最可能的前 num 个密钥
 def group_crack(cipher, num):
-    deviation_table = [(0, 0) for i in range(26)]
+    deviation_list = [(0, 0) for i in range(26)]
     for k in range(26):
-        deviation_table[k] = (chr(ord('a') + k), abs(0.065 - coincidence_index(cipher, k)))
-    deviation_table = sorted(deviation_table, key=lambda x: x[1])
+        deviation_list[k] = (chr(ord('a') + k), abs(0.065 - coincidence_index(cipher, k)))
+    deviation_list = sorted(deviation_list, key=lambda x: x[1])
     for i in range(num):
         if i < num - 1:
-            print(deviation_table[i][0], end='   ')
+            print(deviation_list[i][0], end='   ')
         else:
-            print(deviation_table[i][0])
+            print(deviation_list[i][0])
 
 
 length = 5
