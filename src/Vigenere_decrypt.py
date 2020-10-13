473fbb38c5ea639fc5d211160aa8312abd656606
dongxuluo <dxluo.xdu@gmail.com>
2020-10-13 16:26:10 +0800

    Update Vigenere_decrypt.py

--- src/Vigenere_decrypt.py
+++ src/Vigenere_decrypt.py
@@ -1,16 +1,16 @@
-# 从文件中读入密文
 with open('../static/cipher.txt') as f:
     cipher_text_raw = f.read()
-# 去掉非字母
+
 cipher_text = ''
 for text in cipher_text_raw:
     if text.isalpha():
         cipher_text += text
-
+# 字母表
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
          'w', 'x', 'y', 'z']
 
 
+# 维吉尼亚密码解密
 def decrypt(cipher, key):
     plain_text = ''
     flag = 0
@@ -22,5 +22,7 @@ def decrypt(cipher, key):
     return plain_text
 
 
+vigenere_key = input('请输入密钥\n>')
+# 输出至文件
 with open('../bin/plain.txt', 'w') as f:
-    f.write(decrypt(cipher_text, 'crypt'))
+    f.write(decrypt(cipher_text, vigenere_key))
